d8cf7c28efdff2af622f25ed25ad863c44a685d6
deepakByoSync <deepak.y@byosync.in>
2026-03-22 22:15:48 +0530

    Update API baseURL to use HTTPS for Vercel deployment

--- web-client/src/api/api.js
+++ web-client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "vistasplus-api.vercel.app/api/v1",
+  baseURL: "https://vistasplus-api.vercel.app/api/v1",
   // baseURL: "http://localhost:8000/api/v1",
   // baseURL: "http://localhost:7000/api/v1",
   withCredentials: true,
