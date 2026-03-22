079169acb4cd463dff01719696656be9719f6586
deepakByoSync <deepak.y@byosync.in>
2026-03-22 22:15:22 +0530

    Update API baseURL to Vercel deployment and comment out local development URL

--- web-client/src/api/api.js
+++ web-client/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://byo-sync-backend-testing.vercel.app/api/v1",
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "vistasplus-api.vercel.app/api/v1",
+  // baseURL: "http://localhost:8000/api/v1",
   // baseURL: "http://localhost:7000/api/v1",
   withCredentials: true,
 });
